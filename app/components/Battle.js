30be36e3df7789daa0e1f712e172d08767771a14
Zerryth <ashley.demei.ho@gmail.com>
2018-05-26 12:57:11 -0700

    refectored components App, Battle, Loading, &  Popular to ES6 syntax

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
+const React = require('react');
+const PropTypes = require('prop-types');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
 
 
 PlayerPreview.propTypes = {
@@ -20,13 +20,9 @@ class PlayerInput extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event) {
-        var value = event.target.value;
+        const value = event.target.value;
 
-        this.setState(function () {
-            return {
-                username: value
-            }
-        });
+        this.setState( () => ({ username: value }));
     }
     handleSubmit(event) {
         event.preventDefault();
@@ -37,21 +33,24 @@ class PlayerInput extends React.Component {
         );
     }
     render() {
+        const { username } = this.state;
+        const { label } = this.props;
+
         return (
             <form className='column' onSubmit={this.handleSubmit}>
-                <label className='header' htmlFor='username'>{this.props.label}</label>
+                <label className='header' htmlFor='username'>{label}</label>
                 <input 
                     id='username'
                     placeholder='github username'
                     type='text'
                     autoComplete='off'
-                    value={this.state.username}
+                    value={username}
                     onChange={this.handleChange}
                />
                <button
                     className='button'
                     type='submit'
-                    disabled={!this.state.username}>
+                    disabled={!username}>
                         Submit
                 </button>
             </form>
@@ -83,27 +82,20 @@ class Battle extends React.Component {
         this.handleReset = this.handleReset.bind(this);
     }
     handleReset(id) {
-        this.setState(function (){
-            var newState = {};
-            newState[id + 'Name'] = '';
-            newState[id + 'Image'] = null;
-            return newState;
-        });
+        this.setState( () => ({
+            [id + 'Name']: '',
+            [id + 'Image']: null
+        }));
     }
     handleSubmit(id, username) {
-        this.setState(function() {
-            var newState = {};
-            newState[id + 'Name'] = username;
-            newState[id + 'Image'] = 'http://github.com/' + username + '.png?size=200';
-            return newState;
-        });
+        this.setState(() => ({
+            [id + 'Name']: username,
+            [id + 'Image']: `https://github.com/${username}.png?size=200`
+        }));
     }
     render() {
-        var match = this.props.match;
-        var playerOneName = this.state.playerOneName;
-        var playerTwoName = this.state.playerTwoName;
-        var playerOneImage = this.state.playerOneImage;
-        var playerTwoImage = this.state.playerTwoImage;
+        const { match } = this.props;
+        const { playerOneName, playerOneImage, playerTwoName, playerTwoImage } = this.state;
 
 
         return (
@@ -116,7 +108,7 @@ class Battle extends React.Component {
                         <PlayerPreview avatar={playerOneImage} username={playerOneName}>
                             <button
                                 className='reset'
-                                onClick={this.handleReset.bind(null, 'playerOne')}>
+                                onClick={() => this.handleReset('playerOne')}>
                                 Reset
                             </button>
                         </PlayerPreview>}
@@ -128,7 +120,7 @@ class Battle extends React.Component {
                         <PlayerPreview avatar={playerTwoImage} username={playerTwoName}>
                             <button
                                 className='reset'
-                                onClick={this.handleReset.bind(null, 'playerTwo')}>
+                                onClick={() => this.handleReset('playerTwo')}>
                                 Reset
                             </button>
                         </PlayerPreview>}
@@ -139,7 +131,7 @@ class Battle extends React.Component {
                         className='button'
                         to={{
                             pathname: match.url + '/results',
-                            search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+                            search: `?playerOneName=${playerOneName}&playerTwoName=${playerTwoName}`
                         }}>
                         Battle!
                     </Link>
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,31 +1,24 @@
-var axios = require('axios');
+const axios = require('axios');
 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID"
-var params = "?client_id=" + id + "&client_secret" + sec;
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret${sec}`;
 
 function getProfile (username) {
-    return axios.get('https://api.github.com/users/' + username + params)
-        .then((user) => {
-            return user.data;
-        });
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+        .then(({ data }) => data);
 }
 
 function getRepos (username) {
-    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
 }
 
 function getStarCount (repos) {
-    return repos.data.reduce((count, repo) => {
-        return count + repo.stargazers_count;
-    }, 0);
+    return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
 }
 
-function calculateScore (profile, repos) {
-    var followers = profile.followers;
-    var totalStars = getStarCount(repos);
-
-    return (followers * 3) + totalStars;
+function calculateScore ({ followers }, repos) {
+    return (followers * 3) + getStarCount(repos);
 }
 
 function handleError (error) {
@@ -34,24 +27,15 @@ function handleError (error) {
 }
 
 function getUserData (player) {
-    return axios.all([
-        getProfile(player),
-        getRepos(player)
-    ]).then((data) => {
-        var profile = data[0];
-        var repos = data[1];
-
-        return {
-            profile: profile,
+    return Promise.all([getProfile(player),getRepos(player)])
+    .then(([profile, repos]) => ({
+            profile,
             score: calculateScore(profile, repos)
-        }
-    });
+    }));
 }
 
 function sortPlayers (players) {
-    return players.sort(function (a,b) {
-        return b.score - a.score; 
-    });
+    return players.sort((a,b) => b.score - a.score);
 }
 
 module.exports = {
@@ -61,11 +45,8 @@ module.exports = {
         .catch(handleError)
     },
     fetchPopularRepos (language) {
-        var encodedURI = window.encodeURI(`http://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+        const encodedURI = window.encodeURI(`http://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
 
-        return axios.get(encodedURI)
-            .then((response) => {
-                return response.data.items;
-            });
+        return axios.get(encodedURI).then(({ data }) => data.items);
     }
 }
